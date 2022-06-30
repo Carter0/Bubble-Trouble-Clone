acd9149be38b60d5d9fc3e121676b07761131c5f
carter <weinbergcarter@gmail.com>
2022-06-29 20:06:48 -0400

    Added some basic player code

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use bevy::prelude::*;
 const WINDOWHEIGHT: f32 = 1000.0;
 const WINDOWWIDTH: f32 = 1200.0;
 
+mod logic;
+
 fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
@@ -12,12 +14,19 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(logic::player::PlayerPlugin)
         .add_startup_system(spawn_floor_and_walls)
         .run();
 }
 
-fn spawn_floor_and_walls(mut commands: Commands) {
+#[derive(Component)]
+struct RightWall;
 
+#[derive(Component)]
+struct LeftWall;
+
+// NOTE Origin point in the middle for all transforms
+fn spawn_floor_and_walls(mut commands: Commands) {
     commands
         .spawn()
         .insert_bundle(OrthographicCameraBundle::new_2d());
@@ -39,26 +48,32 @@ fn spawn_floor_and_walls(mut commands: Commands) {
     // The Left Wall
     let left_wall_size_x = 40.0;
     let left_wall_size_y = WINDOWHEIGHT;
-    commands.spawn().insert_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(10.0, 70.0, 70.0),
-            custom_size: Some(Vec2::new(left_wall_size_x, left_wall_size_y)),
+    commands
+        .spawn()
+        .insert_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(10.0, 70.0, 70.0),
+                custom_size: Some(Vec2::new(left_wall_size_x, left_wall_size_y)),
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(-WINDOWWIDTH / 2.0, 0.0, 1.0),
             ..Default::default()
-        },
-        transform: Transform::from_xyz(-WINDOWWIDTH / 2.0, 0.0, 1.0),
-        ..Default::default()
-    });
+        })
+        .insert(LeftWall);
 
     // The Right Wall
     let right_wall_size_x = 40.0;
     let right_wall_size_y = WINDOWHEIGHT;
-    commands.spawn().insert_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(10.0, 70.0, 70.0),
-            custom_size: Some(Vec2::new(right_wall_size_x, right_wall_size_y)),
+    commands
+        .spawn()
+        .insert_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(10.0, 70.0, 70.0),
+                custom_size: Some(Vec2::new(right_wall_size_x, right_wall_size_y)),
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(WINDOWWIDTH / 2.0, 0.0, 1.0),
             ..Default::default()
-        },
-        transform: Transform::from_xyz(WINDOWWIDTH / 2.0, 0.0, 1.0),
-        ..Default::default()
-    });
+        })
+        .insert(RightWall);
 }
