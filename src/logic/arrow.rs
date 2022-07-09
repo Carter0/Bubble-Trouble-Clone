10677b7885033ae3aab2c2df15391e430549eee8
carter <weinbergcarter@gmail.com>
2022-07-09 18:05:38 -0400

    Arrow ball collisions and popping balls

--- src/logic/arrow.rs
+++ src/logic/arrow.rs
@@ -1,8 +1,11 @@
+use crate::logic::ball::{Ball, PopBallEvent};
 use crate::logic::player::Player;
 use crate::{Ceiling, WINDOWHEIGHT, WINDOWWIDTH};
 use bevy::prelude::*;
+use bevy::sprite::collide_aabb::collide;
 
 const ARROWHEIGHT: f32 = 30.0;
+const ARROWWIDTH: f32 = 10.0;
 pub struct ArrowPlugin;
 
 impl Plugin for ArrowPlugin {
@@ -10,7 +13,8 @@ impl Plugin for ArrowPlugin {
         app.add_startup_system(spawn_starting_arrow)
             .add_system(spawn_arrow)
             .add_system(grow_arrow)
-            .add_system(arrow_collisions);
+            .add_system(arrow_ceiling_collisions)
+            .add_system(arrow_ball_collisions);
     }
 }
 
@@ -23,13 +27,11 @@ pub struct Arrow {
 // Spawns an invisible arrow in the beginning of the game
 // so that there is always one arrow in the game
 fn spawn_starting_arrow(mut commands: Commands) {
-    let arrow_width = 10.0;
-
     commands
         .spawn_bundle(SpriteBundle {
             sprite: Sprite {
                 color: Color::rgb(10.0, 70.0, 70.0),
-                custom_size: Some(Vec2::new(arrow_width, ARROWHEIGHT)),
+                custom_size: Some(Vec2::new(ARROWWIDTH, ARROWHEIGHT)),
                 ..Default::default()
             },
             transform: Transform::from_xyz(WINDOWWIDTH, WINDOWHEIGHT, 1.0),
@@ -82,7 +84,7 @@ fn grow_arrow(mut arrow_query: Query<(&mut Transform, &Arrow), With<Arrow>>) {
 }
 
 // If the arrow collides with the ceiling it goes off screen
-fn arrow_collisions(
+fn arrow_ceiling_collisions(
     mut arrow_query: Query<(&mut Arrow, &mut Transform), (With<Arrow>, Without<Ceiling>)>,
     ceiling_query: Query<&Transform, With<Ceiling>>,
 ) {
@@ -99,10 +101,34 @@ fn arrow_collisions(
     // We calculate the top of the arrow by multiplying it by the scale
     // and dividing a bunch because the origin point of everything is always
     // in the middle
-    if arrow_transform.scale.y * ARROWHEIGHT / 4.0
-        >= ceiling_transform.translation.y - 20.0
-    {
+    if arrow_transform.scale.y * ARROWHEIGHT / 4.0 >= ceiling_transform.translation.y - 20.0 {
         arrow.destroyed = true;
         arrow_transform.translation.x = WINDOWWIDTH * 2.0;
+    }
+}
+
+// If the arrow collides with the ball it goes off screen and the ball gets popped
+fn arrow_ball_collisions(
+    mut arrow_query: Query<(&mut Arrow, &mut Transform), (With<Arrow>, Without<Ball>)>,
+    ball_query: Query<(&Transform, &Ball, Entity), With<Ball>>,
+    mut pop_ball_event: EventWriter<PopBallEvent>,
+) {
+    let (mut arrow, mut arrow_transform) = arrow_query
+        .get_single_mut()
+        .expect("Error: Could not find a single arrow.");
+
+    let arrow_size_x = arrow_transform.scale.x * ARROWWIDTH;
+    let arrow_size_y = arrow_transform.scale.y * ARROWHEIGHT;
+    for (ball_transform, ball, ball_entity) in ball_query.iter() {
+        if let Some(_collision) = collide(
+            ball_transform.translation,
+            Vec2::new(ball.side, ball.side),
+            arrow_transform.translation,
+            Vec2::new(arrow_size_x, arrow_size_y),
+        ) {
+            arrow.destroyed = true;
+            arrow_transform.translation.x = WINDOWWIDTH * 2.0;
+            pop_ball_event.send(PopBallEvent(ball_entity));
+        }
     }
 }
--- src/logic/ball.rs
+++ src/logic/ball.rs
@@ -1,14 +1,27 @@
 use crate::{Floor, LeftWall, RightWall};
 use bevy::prelude::*;
 pub struct BallPlugin;
+use std::collections::HashMap;
 
 const GRAVITY: f32 = -0.1;
 
+#[derive(Default)]
+struct BallMappings {
+    ball_sizes: HashMap<u8, f32>,
+    bounce_velocities: HashMap<u8, f32>,
+}
+
 impl Plugin for BallPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_starting_ball)
-            .add_system(move_balls)
-            .add_system(bounce_balls);
+        app.insert_resource(BallMappings {
+            ball_sizes: HashMap::from([(1, 20.0), (2, 40.0), (3, 60.0), (4, 80.0), (5, 100.0)]),
+            bounce_velocities: HashMap::from([(1, 6.0), (2, 8.0), (3, 10.0), (4, 12.0), (5, 14.0)]),
+        })
+        .add_startup_system(spawn_starting_ball)
+        .add_system(move_balls)
+        .add_system(bounce_balls)
+        .add_event::<PopBallEvent>()
+        .add_system(pop_ball);
     }
 }
 
@@ -16,14 +29,24 @@ impl Plugin for BallPlugin {
 pub struct Ball {
     y_velocity: f32,
     x_velocity: f32,
+    // All balls should be squares
+    // TODO im not sure I need this
+    pub side: f32,
+    // Range from 1-5.
+    // The higher numbers would indicate a larger ball
+    // If size is 1, then it is the smallest ball and can be destroyed if hit
+    size: u8,
 }
 
-fn spawn_starting_ball(mut commands: Commands) {
+fn spawn_starting_ball(mut commands: Commands, ball_sizes_res: Res<BallMappings>) {
+    let ball_size = 2;
+    let ball_side = ball_sizes_res.ball_sizes[&2];
+
     commands
         .spawn_bundle(SpriteBundle {
             sprite: Sprite {
                 color: Color::rgb(10.0, 70.0, 70.0),
-                custom_size: Some(Vec2::new(40.0, 40.0)),
+                custom_size: Some(Vec2::new(ball_side, ball_side)),
                 ..Default::default()
             },
             transform: Transform::from_xyz(0.0, 0.0, 1.0),
@@ -32,6 +55,8 @@ fn spawn_starting_ball(mut commands: Commands) {
         .insert(Ball {
             y_velocity: 0.0,
             x_velocity: 1.0,
+            side: ball_side,
+            size: ball_size,
         });
 }
 
@@ -48,6 +73,7 @@ fn bounce_balls(
     right_wall_query: Query<&Transform, (With<RightWall>, Without<Ball>)>,
     left_wall_query: Query<&Transform, (With<LeftWall>, Without<Ball>)>,
     mut ball_query: Query<(&Transform, &mut Ball)>,
+    ball_sizes_res: Res<BallMappings>,
 ) {
     let floor_transform = floor_query
         .get_single()
@@ -63,16 +89,83 @@ fn bounce_balls(
 
     // NOTE ball is 40 by 40
     for (ball_transform, mut ball) in ball_query.iter_mut() {
-        if ball_transform.translation.y - 20.0 <= floor_transform.translation.y + 20.0 {
-            ball.y_velocity *= -1.0;
+        if ball_transform.translation.y - ball.side / 2.0 <= floor_transform.translation.y + 20.0 {
+            // NOTE each size of ball only bounces a specific height
+            let ball_bounce = ball_sizes_res.bounce_velocities[&ball.size];
+            ball.y_velocity = ball_bounce;
         }
 
-        if ball_transform.translation.x + 20.0 >= right_wall_transform.translation.x - 20.0 {
+        if ball_transform.translation.x + ball.side / 2.0
+            >= right_wall_transform.translation.x - 20.0
+        {
             ball.x_velocity *= -1.0;
         }
 
-        if ball_transform.translation.x - 20.0 <= left_wall_transform.translation.x + 20.0 {
+        if ball_transform.translation.x - ball.side / 2.0
+            <= left_wall_transform.translation.x + 20.0
+        {
             ball.x_velocity *= -1.0;
         }
+    }
+}
+
+pub struct PopBallEvent(pub Entity);
+
+// When a ball gets popped spawn two smaller balls
+// unless the ball is as small as it can go
+fn pop_ball(
+    mut pop_ball_event: EventReader<PopBallEvent>,
+    mut commands: Commands,
+    ball_query: Query<(&Ball, &Transform)>,
+    ball_sizes_res: Res<BallMappings>,
+) {
+    for pop_ball_event in pop_ball_event.iter() {
+        let ball_entity = pop_ball_event.0;
+
+        if let Ok((ball, ball_transform)) = ball_query.get(ball_entity) {
+            // if the ball can still get smaller
+            if ball.size - 1 > 0 {
+                let new_ball_size = ball.size - 1;
+                let new_ball_side = ball_sizes_res.ball_sizes[&(new_ball_size)];
+
+                // NOTE give each ball a 4 point lift so that
+                // its possible they can bounce to the top of the screen
+                commands
+                    .spawn_bundle(SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::rgb(10.0, 70.0, 70.0),
+                            custom_size: Some(Vec2::new(new_ball_side, new_ball_side)),
+                            ..Default::default()
+                        },
+                        transform: *ball_transform,
+                        ..Default::default()
+                    })
+                    .insert(Ball {
+                        y_velocity: 4.0,
+                        x_velocity: 1.0,
+                        side: new_ball_side,
+                        size: new_ball_size,
+                    });
+
+                commands
+                    .spawn_bundle(SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::rgb(10.0, 70.0, 70.0),
+                            custom_size: Some(Vec2::new(new_ball_side, new_ball_side)),
+                            ..Default::default()
+                        },
+                        transform: *ball_transform,
+                        ..Default::default()
+                    })
+                    .insert(Ball {
+                        y_velocity: 4.0,
+                        x_velocity: -1.0,
+                        side: new_ball_side,
+                        size: new_ball_size,
+                    });
+            }
+        }
+
+        commands.entity(ball_entity).despawn();
     }
 }
